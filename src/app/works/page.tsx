4892100fc4884c005d5d907314be798e37168f09
samandareo <smdeveloper04@gmail.com>
2025-03-17 18:44:54 +0500

    Done work experience in WORKS page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,10 +1,28 @@
 import ContainerHeader from "@/components/ContainerHeader";
 import ContainerFooter from "@/components/ContainerFooter";
+import ContainerExperience from "@/components/ContainerExperience";
 
 export default function Home() {
   return (
-      <div className="flex flex-col gap-y-36">
+      <div className="flex flex-col gap-y-36 w-10/12 mx-auto">
         <ContainerHeader/>
+        <div className="container mx-auto flex flex-col gap-y-4">
+          <div>
+            <div>
+              <h1 className="text-2xl font-semibold">Experience</h1>
+              <div className="w-100 h-[0.5px] bg-white mt-2 opacity-50"></div>
+            </div>
+            <div className="flex flex-col gap-y-4 mt-4">
+              <ContainerExperience/>
+              <ContainerExperience/>
+            </div>
+          </div>
+          
+          <div>
+
+          </div>
+
+        </div>
         <ContainerFooter />
       </div>
 )
---
+++ src/components/ContainerExperience.tsx
@@ -0,0 +1,18 @@
+
+
+export default function ContainerExperience() {
+    return (
+        <div className="flex flex-col gap-y-2 border-2 border-[#1E3A8A] rounded-lg p-4 bg-[rgba(30,58,138,0.2)]">
+            <div className="flex flex-row justify-between items-center">
+                <span className="flex flex-row gap-x-2 items-center flex-wrap">
+                    <h2 className="text-lg font-semibold">UI/UX designer</h2>
+                    <h3 className="text-sm font-extralight">(PDP ACADEMY)</h3>
+                </span>
+                <span>
+                    <h4 className="text-sm font-light">Nov 2023 - Feb 2024</h4>
+                </span>
+            </div>
+            <p className="text-sm font-light">Lorem ipsum dolor, dolor is color, so we need to go to folor, you know and me know color. Lorem ipsum dolor, dolor is color, so we need to go to folor, you know and me know color. Lorem ipsum dolor, dolor is color, so we need to go to folor, you know and me know color.</p>
+        </div>
+    )
+}
